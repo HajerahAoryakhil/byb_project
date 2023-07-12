432f09cbe4dbadfd5d23a20b6a54335f32e90b1d
HajerahAoryakhil <haoryakhil@gmail.com>
2023-07-12 22:06:00 +0100

    My name is Hajerah

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#Task 1 
+
+#In this task its asking me to take both user name and age and print them 
+#To do so I made two variable one for name and one for age and used the input() command 
+#Then I printed them both out
+#Next its asking me to print out Hello World! in a new line 
+#To get a new line I use the characters "\n" in the print() function
+#This will make a newline and print out the string Hello World!
+
+user_name = input("What is your name?" )
+user_age = input("How old are you?" )
+print(user_name)
+print(user_age) 
+print("\n Hello World!")
